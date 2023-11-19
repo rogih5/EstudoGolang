fbdf8201e078db3c6fd443ef4f88782af45c7efe
Higor de jesus <71574120+rogih5@users.noreply.github.com>
2023-11-18 22:48:13 -0300

    ESTUDANDO GO

---
+++ hello.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+// Hello, World (A QUEBRA DA MALDIÇÃO)
+func main() {
+	fmt.Println("Hello, World")
+}
+
+//Estrutura de repetição
+// func main() {
+// 	alunos := []string{"Cleyson", "Elton", "Sara", "Wesley", "Anna"}
+// 	for _, aluno := range alunos {
+// 		fmt.Println(aluno)
+// 	}
+// }
+
+//tradicional do uso da estrutura if/else
+// func main() {
+// 	idade := 18
+// 	if idade >= 18 {
+// 		fmt.Println("Você é maior de idade")
+// 	} else {
+// 		fmt.Println("Você é menor de idade")
+// 	}
+// }
+
+// AQUI TEMOS ADIÇÂO MULTIPLICAÇÃO SUBTRAÇÃO DIVISÃO
+// func main() {
+// 	resultado := multiplicacao(10, 2)
+// 	resultado1 := divisao(10, 2)
+// 	resultado2 := adicao(10, 2)
+// 	resultado3 := subtracao(10, 2)
+
+// 	fmt.Println(resultado, resultado1, resultado2, resultado3)
+// }
+
+// func multiplicacao(num1 int, num2 int) int {
+// 	return num1 * num2
+// }
+// func divisao(num1 int, num2 int) int {
+// 	return num1 / num2
+// }
+// func adicao(num1 int, num2 int) int {
+// 	return num1 + num2
+// }
+// func subtracao(num1 int, num2 int) int {
+// 	return num1 - num2
+// }
